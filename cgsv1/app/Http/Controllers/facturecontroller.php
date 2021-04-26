dd856d5e63cee20176c8c69a2efddcd128100976
oussama <81413348+0912-ouss@users.noreply.github.com>
2021-04-26 23:47:47 +0000

    Create facturecontroller.php

---
+++ cgsv1/app/Http/Controllers/facturecontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class facturecontroller extends Controller
+{
+    //
+    function getall(){
+        return"oussama";
+    }
+}
