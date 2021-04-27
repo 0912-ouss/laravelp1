de57247ec2128369ec2b9721a7b4c2a3de99a2cb
oussama <81413348+0912-ouss@users.noreply.github.com>
2021-04-27 22:10:58 +0000

    Update cfacture.php

--- cgsv1/app/Http/Controllers/cfacture.php
+++ cgsv1/app/Http/Controllers/cfacture.php
@@ -11,6 +11,25 @@ class cfacture extends Controller
     public function getfacture(){
      
        // return DB::select('select * from facture');
-        return DB::table('facture')->get();
+        return DB::table('facture')
+        //->count();
+        ->get();
     }
+    public function getone($num){
+     
+        // return DB::select('select * from facture');
+         return DB::table('facture')
+         ->where('numF',$num)
+         ->get();
+     }
+     public function ajfact(Request $r){
+     
+        // return DB::select('select * from facture');
+        DB::table('facture')->insert(["numF"]=>$r['numf'],);
+         return DB::table('facture');
+         
+         
+     }
 }
+
+
