02adf786443889f471bfb514a9fad8bbf07f17f9
oussama <81413348+0912-ouss@users.noreply.github.com>
2021-04-29 09:55:57 +0000

    Update allfacture.blade.php

--- cgsv1/resources/views/allfacture.blade.php
+++ cgsv1/resources/views/allfacture.blade.php
@@ -6,51 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>produit admin</title>
-    <link rel="stylesheet" href="css/produit.css">
-    <link rel="stylesheet" href="all.css">
-    <link rel="stylesheet" href="css/forni.css">
+
 </head>
 
 <body>
     
     <main>
-        <div class="main-title">
-
-            <p>home cart</p>
-            <span> my cart</span>
-
-        </div>
-        <form class="forni">
+        
+        <form  method="GEt" action="">
             <ul>
                 <li>
-                    <p id="p1">action</p>
-                    <p id="p2">nom</p>
-                    <p id="p3">email</p>
-                    <p id="p4">tel</p>
-                    <p id="p6">adress</p>
-                </li>
-                <li>
-                    <div id="p1" class="action">
-                        <span> <button><i class="far fa-times"></i></button></span>
-                    </div>
-                    <p id="p2">oussama berhayla</p>
-                    <div id="p3">
-                        <h1>maoussaberhayla@gmail.com</h1>
-                    </div>
-                    <p id="p4" class="p7">0987654321</p>
-                    <p id="p6" class="p7">7 rue kanar hay tarik 2</p>
-                </li>
-                <li>
-                    <div id="p1" class="action">
-                        <span> <button><i class="far fa-times"></i></button></span>
-                    </div>
-                    <p id="p2">oussama berhayla</p>
-                    <div id="p3">
-                        <h1>maoussaberhayla@gmail.com</h1>
-                    </div>
-                    <p id="p4" class="p7">0987654321</p>
-                    <p id="p6" class="p7">7 rue kanar hay tarik 2</p>
-                </li>
+                <p>num</p>
+                <p>objet</p>
+                <p>date</p>
+                <p>client</p>
+                <p>taux tva</p>
+                <p>prix ht</p>
+                <p>tva</p>
+                <p>prix ttc</p>
+            </li>
+            @foreach ($dt as $o )
+            <li>
+                <p>{{ $o->numF }}</p>
+                <p>{{ $o->objet }}</p>
+                <p>{{ $o->dateF }}</p>
+                <p>{{ $o->client }}</p>
+                <p>{{ $o->tauxTva }}</p>
+                <p>{{ $o->prixHt }}</p>
+                <p>{{ $o->tva }}</p>
+                <p>{{ $o->prixTtc }}</p>
+            </li>
+            @endforeach
+                
+
+                
+             
                 
                 
                 
@@ -58,10 +48,7 @@
             </ul>
 
         </form>
-        <form class="order">
-            <button class="btn">add Fornisseur</button>
-            <button class="btn">edit fornisseur</button>
-        </form>
+       
 
     </main>
    
