ce06689a2a3657e563b1bc2b5e64df008a37b5af
oussama <81413348+0912-ouss@users.noreply.github.com>
2021-04-30 23:09:44 +0000

    Update upfacture.blade.php

--- cgsv1/resources/views/upfacture.blade.php
+++ cgsv1/resources/views/upfacture.blade.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sign up</title>
+    <title>update facture</title>
     <link rel="stylesheet" href="../css/all.css">
     <link rel="stylesheet" href="../css/login.css">
 </head>
 
 <body>
+   
 
 
     <div class="wrapper">
@@ -27,41 +28,52 @@
         <!-- end quote-wrapper -->
 
         <div class="form-wrapper">
-            <h1 class="form-title">update FACTURE num {{ $o->numF }}</h1>
+            <h1 class="form-title">update FACTURE</h1>
 
             <form method="GET" action="save">
+                @php
+                if(isset($o)){
+                    
+                    $nume=$o->numF;
+                    $objet=$o->objet;
+                  $date=$o->dateF;
+                 $client= $o->client;
+                 $tva= $o->tauxTva;
+                 
+                } 
+              @endphp
                 @csrf
                 <div class="form-group">
                     <label for="username">num facture</label>
-                    <input type="text" name="numf" id="username" class="w-100" autofocus value="{{ $o->numF }}">
+                    <input type="text" name="numf" id="username" class="w-100"  value="{{ $nume }}">
                     <span class="check-icon"></span>
                 </div>
 
                 <div class="form-group">
                     <label for="email">date</label>
-                    <input type="date"name="date" id="email" class="w-100" value="{{ $o->dateF }}">
+                    <input type="date"name="date" id="email" class="w-100" value="{{ $date}}">
                     <span class="check-icon"></span>
                 </div>
 
                 <div class="form-group ">
                     <label for="password">client</label>
-                    <input type="text" name="client" id="password" class="w-100" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" value="{{ $o->client }}">
+                    <input type="text" name="client" id="password" class="w-100" title="Must contain characters" value="{{ $client }}">
                     <span class="check-icon"></span>
                 </div>
                 <div class="form-group">
                     <label for="username">tva</label>
-                    <input type="text" name="tva" id="username" class="w-100" autofocus value="{{ $o->tauxTva }}" title="Must be longer than 4 characters">
+                    <input type="text" name="tva" id="username" class="w-100" autofocus value="{{ $tva }}" title="Must be longer than 4 characters">
                     <span class="check-icon"></span>
                 </div>
                 <div class="form-group">
                     <label for="username">objet</label>
-                    <input type="text" name="objet" id="username" class="w-100" autofocus value="{{ $o->objet }}" title="Must be longer than 4 characters">
+                    <input type="text" name="objet" id="username" class="w-100" autofocus value="{{ $objet }}" title="Must be longer than 4 characters">
                     <span class="check-icon"></span>
                 </div>
 
 
                 <div class="form-group mb-0">
-                    <button class="btn" name="save"> save</button>
+                    <button class="btn" name="se"> save</button>
                 </div>
             </form>
 
