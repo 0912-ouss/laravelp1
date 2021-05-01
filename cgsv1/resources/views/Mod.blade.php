7820b0f20299f0b32aa94f6c1784190b25c6770e
oussama <81413348+0912-ouss@users.noreply.github.com>
2021-05-01 14:11:21 +0000

    Create Mod.blade.php

---
+++ cgsv1/resources/views/Mod.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="AjFact" method="GET" class="AjMod">
+@php
+if(isset($d))
+{
+   // $des=$d->qte;
+    $op="update";
+}
+@endphp
+
+    <div class="par">
+
+        <span>Designation</span><input type="text" name="des" id="">
+        <span>Unite</span><input type="text" name="uni" id="">
+        <span>Qte</span><input type="text" name="qte" id="" >
+    
+        <span>Prix Unitaire</span><input type="text" name="pu" id="">
+        <span>Prix Totale</span><input type="text" name="pt" id="">
+        <div>    
+            <button type="submit" name="save">UPDATE</button>
+            <a href="AjMod" name="Rest">Rest</a>
+            <a href="Detaillfact" name="end">End</a>
+        </div>
+    </div>
+
+</form>
+
+</body>
+</html>
