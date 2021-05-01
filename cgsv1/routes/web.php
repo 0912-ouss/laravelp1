7a095d061b4abc72a8f525df60eb694eb1a7f44d
oussama <81413348+0912-ouss@users.noreply.github.com>
2021-05-01 14:11:18 +0000

    Update web.php

--- cgsv1/routes/web.php
+++ cgsv1/routes/web.php
@@ -34,5 +34,23 @@ Route::get('ajfact', 'App\Http\Controllers\cfacture@ajfact');
 Route::get('addfactures', function () {
     return view('addfactures');
 });
+Route::get('AjMod', function () {
+    return view('AjMod');
+});
 
+Route::get('AjMod', function () {
+    return view('AjMod');
+});
+
+Route::get('Mod', function () {
+    return view('Mod');
+});
+Route::get('Detaillfact', function () {
+    return view('Detaillfact');
+});
+
+Route::get('AjFact','App\Http\Controllers\facteurat@ajouter');
+Route::get('SuDFact/{num}','App\Http\Controllers\facteurat@supprimer');
+Route::get('UpDFact/{num}','App\Http\Controllers\facteurat@update');
+Route::get('Detaillfact','App\Http\Controllers\DFact@GetAll');
 
